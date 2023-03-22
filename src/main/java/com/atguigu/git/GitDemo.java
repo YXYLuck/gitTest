e41ee4962ba7b7736e792ea61f0e83191e08cbda
yxy <yxy@qqcomexit>
2023-03-22 17:45:49 +0800

    push first commit

--- src/main/java/com/atguigu/git/GitDemo.java
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git3 hotFix");
+        System.out.println("push hotFix");
     }
 }
