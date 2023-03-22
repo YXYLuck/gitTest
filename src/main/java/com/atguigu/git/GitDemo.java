eb99707ea3054f2a2ea3058e6a298d60d5f158b7
yxy <yxy@qqcomexit>
2023-03-22 19:48:22 +0800

    gitee test

--- src/main/java/com/atguigu/git/GitDemo.java
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -8,5 +8,6 @@ public class GitDemo {
         System.out.println("hello git3 hotFix");
         System.out.println("push hotFix");
         System.out.println("pull test3");
+        System.out.println("gitee test3");
     }
 }
